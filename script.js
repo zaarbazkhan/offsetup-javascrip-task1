1724bf7563355ab135d712b183c44514482f16e5
My Name <khanarba80808@gmail.com>
2024-08-16 15:28:58 +0500

    your message

---
+++ script.js
@@ -0,0 +1,372 @@
+var arr1 = [
+    {
+      id: 1,
+      first_name: "Kathi",
+      last_name: "Clemendot",
+      email: "kclemendot0@addtoany.com",
+      gender: "Genderqueer",
+      ip_address: "43.88.213.195",
+    },
+    {
+      id: 2,
+      first_name: "Ambrosi",
+      last_name: "Pierri",
+      email: "apierri1@china.com.cn",
+      gender: "Genderfluid",
+      ip_address: "138.237.147.69",
+    },
+    {
+      id: 3,
+      first_name: "Jamill",
+      last_name: "Waddell",
+      email: "jwaddell2@about.me",
+      gender: "Bigender",
+      ip_address: "39.122.158.234",
+    },
+    {
+      id: 4,
+      first_name: "Susanna",
+      last_name: "Nichol",
+      email: "snichol3@yolasite.com",
+      gender: "Genderqueer",
+      ip_address: "125.19.137.250",
+    },
+    {
+      id: 5,
+      first_name: "Gardiner",
+      last_name: "Ivanonko",
+      email: "givanonko4@umn.edu",
+      gender: "Bigender",
+      ip_address: "56.193.188.214",
+    },
+    {
+      id: 6,
+      first_name: "Andros",
+      last_name: "Haselden",
+      email: "ahaselden5@mapquest.com",
+      gender: "Non-binary",
+      ip_address: "100.57.97.51",
+    },
+    {
+      id: 7,
+      first_name: "Sindee",
+      last_name: "Renny",
+      email: "srenny6@reference.com",
+      gender: "Bigender",
+      ip_address: "208.167.187.166",
+    },
+    {
+      id: 8,
+      first_name: "Gaspar",
+      last_name: "Dabner",
+      email: "gdabner7@mlb.com",
+      gender: "Agender",
+      ip_address: "141.240.11.173",
+    },
+    {
+      id: 9,
+      first_name: "Kerrill",
+      last_name: "Beldum",
+      email: "kbeldum8@yellowpages.com",
+      gender: "Polygender",
+      ip_address: "244.255.44.52",
+    },
+    {
+      id: 10,
+      first_name: "Emmey",
+      last_name: "Kellick",
+      email: "ekellick9@a8.net",
+      gender: "Polygender",
+      ip_address: "223.62.177.29",
+    },
+    {
+      id: 11,
+      first_name: "Sophey",
+      last_name: "Bendik",
+      email: "sbendika@aol.com",
+      gender: "Polygender",
+      ip_address: "85.209.42.189",
+    },
+    {
+      id: 12,
+      first_name: "Ricardo",
+      last_name: "Macklin",
+      email: "rmacklinb@tmall.com",
+      gender: "Bigender",
+      ip_address: "79.184.214.236",
+    },
+    {
+      id: 13,
+      first_name: "Belita",
+      last_name: "Gillbanks",
+      email: "bgillbanksc@noaa.gov",
+      gender: "Genderqueer",
+      ip_address: "59.254.24.113",
+    },
+    {
+      id: 14,
+      first_name: "Ema",
+      last_name: "Derisley",
+      email: "ederisleyd@nhs.uk",
+      gender: "Female",
+      ip_address: "197.212.94.171",
+    },
+    {
+      id: 15,
+      first_name: "Eden",
+      last_name: "Willerson",
+      email: "ewillersone@com.com",
+      gender: "Genderfluid",
+      ip_address: "216.45.210.221",
+    },
+    {
+      id: 16,
+      first_name: "Genvieve",
+      last_name: "Pepperrall",
+      email: "gpepperrallf@ucsd.edu",
+      gender: "Polygender",
+      ip_address: "4.194.72.133",
+    },
+    {
+      id: 17,
+      first_name: "Calhoun",
+      last_name: "Theuss",
+      email: "ctheussg@pen.io",
+      gender: "Bigender",
+      ip_address: "193.229.202.100",
+    },
+    {
+      id: 18,
+      first_name: "Raffarty",
+      last_name: "Sandes",
+      email: "rsandesh@bloglines.com",
+      gender: "Genderfluid",
+      ip_address: "81.249.240.110",
+    },
+    {
+      id: 19,
+      first_name: "Kerstin",
+      last_name: "Luc",
+      email: "kluci@house.gov",
+      gender: "Non-binary",
+      ip_address: "55.169.205.231",
+    },
+    {
+      id: 20,
+      first_name: "Evie",
+      last_name: "Lomen",
+      email: "elomenj@mapy.cz",
+      gender: "Genderfluid",
+      ip_address: "133.88.62.83",
+    },
+  ];
+
+
+var arr2 = [
+    {
+      id: 1,
+      first_name: "Allys",
+      last_name: "Cokly",
+      email: "acokly0@businesswire.com",
+      gender: "Agender",
+      ip_address: "27.100.43.23",
+    },
+    {
+      id: 2,
+      first_name: "Rog",
+      last_name: "Gibbie",
+      email: "rgibbie1@dropbox.com",
+      gender: "Polygender",
+      ip_address: "86.38.1.206",
+    },
+    {
+      id: 3,
+      first_name: "Garwood",
+      last_name: "Rayman",
+      email: "grayman2@cafepress.com",
+      gender: "Bigender",
+      ip_address: "43.235.10.140",
+    },
+    {
+      id: 4,
+      first_name: "Paulina",
+      last_name: "Cowdery",
+      email: "pcowdery3@cnbc.com",
+      gender: "Non-binary",
+      ip_address: "160.214.34.226",
+    },
+    {
+      id: 5,
+      first_name: "Jude",
+      last_name: "Dimmock",
+      email: "jdimmock4@123-reg.co.uk",
+      gender: "Non-binary",
+      ip_address: "23.154.215.149",
+    },
+    {
+      id: 6,
+      first_name: "Betteanne",
+      last_name: "Croxon",
+      email: "bcroxon5@youtube.com",
+      gender: "Polygender",
+      ip_address: "185.147.168.176",
+    },
+    {
+      id: 7,
+      first_name: "Chris",
+      last_name: "Norkutt",
+      email: "cnorkutt6@nbcnews.com",
+      gender: "Genderqueer",
+      ip_address: "159.189.206.89",
+    },
+    {
+      id: 8,
+      first_name: "Devinne",
+      last_name: "Coady",
+      email: "dcoady7@forbes.com",
+      gender: "Genderfluid",
+      ip_address: "203.73.88.113",
+    },
+    {
+      id: 9,
+      first_name: "Banky",
+      last_name: "Barnaclough",
+      email: "bbarnaclough8@feedburner.com",
+      gender: "Male",
+      ip_address: "142.35.19.236",
+    },
+    {
+      id: 10,
+      first_name: "Brandie",
+      last_name: "Baselli",
+      email: "bbaselli9@ebay.com",
+      gender: "Genderfluid",
+      ip_address: "64.136.43.191",
+    },
+    {
+      id: 11,
+      first_name: "Isidor",
+      last_name: "McWilliams",
+      email: "imcwilliamsa@etsy.com",
+      gender: "Polygender",
+      ip_address: "40.112.141.55",
+    },
+    {
+      id: 12,
+      first_name: "Tricia",
+      last_name: "Cumes",
+      email: "tcumesb@nymag.com",
+      gender: "Male",
+      ip_address: "238.125.139.189",
+    },
+    {
+      id: 13,
+      first_name: "Opal",
+      last_name: "Cersey",
+      email: "ocerseyc@answers.com",
+      gender: "Non-binary",
+      ip_address: "48.204.37.115",
+    },
+    {
+      id: 14,
+      first_name: "Anabella",
+      last_name: "Escoffrey",
+      email: "aescoffreyd@clickbank.net",
+      gender: "Genderqueer",
+      ip_address: "31.7.66.200",
+    },
+    {
+      id: 15,
+      first_name: "Rodina",
+      last_name: "O' Donohoe",
+      email: "rodonohoee@utexas.edu",
+      gender: "Agender",
+      ip_address: "122.125.62.236",
+    },
+    {
+      id: 16,
+      first_name: "Missie",
+      last_name: "Symones",
+      email: "msymonesf@ocn.ne.jp",
+      gender: "Female",
+      ip_address: "157.127.165.188",
+    },
+    {
+      id: 17,
+      first_name: "Brantley",
+      last_name: "Carous",
+      email: "bcarousg@yelp.com",
+      gender: "Male",
+      ip_address: "128.122.91.96",
+    },
+    {
+      id: 18,
+      first_name: "Grissel",
+      last_name: "Shanahan",
+      email: "gshanahanh@cbslocal.com",
+      gender: "Polygender",
+      ip_address: "161.13.72.171",
+    },
+    {
+      id: 19,
+      first_name: "Orran",
+      last_name: "Hardisty",
+      email: "ohardistyi@biblegateway.com",
+      gender: "Male",
+      ip_address: "123.253.208.29",
+    },
+    {
+      id: 20,
+      first_name: "Rob",
+      last_name: "Abeles",
+      email: "rabelesj@360.cn",
+      gender: "Agender",
+      ip_address: "111.113.58.161",
+    },
+  ];
+
+  // merge arr1 and arr 2
+  var mergedArray = [...arr1, ...arr2];
+  console.log(mergedArray);
+  
+  // Remove the user with id 5
+
+  mergedArray.splice(5, 1);
+  
+
+  for (var i = 0; i < mergedArray.length; i++) {
+    if (mergedArray[i].email === "bgillbanks@noaa.gov") {
+        mergedArray[i].email = "user@offsetup.com";
+       break;
+    }
+}
+
+
+
+
+
+
+// find even numbers and the sum of the array
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Find even numbers
+const evenNumbers = [];
+
+for (const num of numbers) {
+    if (num % 2 === 0) {
+        evenNumbers.push(num);
+    }
+}
+
+console.log("Even Numbers:", evenNumbers); // Output: [2, 4, 6, 8, 10]
+
+//  sum of the array
+let sum = 0;
+
+for (const num of numbers) {
+    sum += num;
+}
+
+console.log("Sum of Array:", sum); // Output: 55
+
