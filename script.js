170e604d067a7b15a7bf3eae53b4a0f62b14804c
My Name <khanarba80808@gmail.com>
2024-08-19 12:46:24 +0500

    updated

--- script.js
+++ script.js
@@ -385,6 +385,6 @@ console.log("Even Numbers:", evenNumbers); // Output: [2, 4, 6, 8, 10]
 // sum of the array
 const arr = [1,2,3,4,5,6,7,8,9,10];
 const array2 = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log(array2);
+console.log(array2); // Output: 55
 
 
